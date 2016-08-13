5489db3489798f857c5b5e92498f43f23c66c364
kevin <k_upton@live.com>
2016-08-14 02:49:53 +1000

    added stuff

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import middleware from './middleware';
 import api from './api';
 import config from './config.json';
 import path from 'path';
+import socketio from 'socket.io';
+
+
 
 let app = express();
 app.server = http.createServer(app);
+let io = socketio(app.server);
 
 //app.use(express.static(path.join(__dirname, 'public')));
 //
@@ -23,23 +27,42 @@ app.server = http.createServer(app);
 //}));
 //
 //// connect to db
-//initializeDb( db => {
-//
-//	// internal middleware
-//	app.use(middleware({ config, db }));
-//
-//	// api router
-//	app.use('/api', api({ config, db }));
-//
-//	app.server.listen(process.env.PORT || config.port);
-//
-//	console.log(`Started on port ${app.server.address().port}`);
-//});
+initializeDb( db => {
 
+	// internal middleware
+	app.use(middleware({ config, db }));
+
+	// api router
+    app.use('/api', api({ config, db }));
+
+});
+
+app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
 	res.sendfile('public/index.html', {root: __dirname});
 });
-app.server.listen(8080);
+
+app.server.listen(process.env.PORT || config.port);
+
+console.log(`Started on port ${app.server.address().port}`);
+
+const sockets = [];
+let character = {};
+
+io.on('connection', function (socket) {
+    sockets.push(socket);
+    socket.emit('msg', 'Welcome to static swag online!');
+    socket.emit('character', character);
+    socket.on('character', function (data) {
+        character = data;
+        sockets.filter(val => val !== socket).forEach(val => {
+            val.emit('character', character);
+        });
+    });
+    socket.on('msg', msg => {
+        sockets.filter(val => val !== socket).forEach(val => val.emit('msg', msg));
+    });
+});
 
 export default app;
--- src/public/index.js
+++ src/public/index.js
@@ -1,14 +1,49 @@
 (() => {
     const self = this;
 
+    const character = {};
+    Object.defineProperties(window, {
+        'game': {
+            value: proxyObject(character, character)
+        },
+        'msg': {
+            value: msg
+        }
+    });
 
-    console.log(window);
+    socket.on('character', data => {
+        window.character = proxyObject(data, data);
+    });
 
-    window.__defineGetter__('test', () => console.log('test'));
+    socket.on('msg', msg => {
+        console.info('msg: ' + msg);
+    });
 
-
-    var proxy = {
-        get: () => console.log('getting')
+    function socketProxy (object) {
+        return {
+            //get: () => console.log('get'),
+            set: (target, key, val) => {
+                if (typeof val === 'object') {
+                    val = proxyObject(val, object);
+                }
+                target[key] = val;
+                socket.emit('character', object);
+            },
+            deleteProperty: (target, prop) => {
+                delete target[prop];
+                socket.emit('character', object);
+            }
+        }
     }
 
+    function proxyObject (object, parent) {
+        Object.keys(object)
+            .filter(key => typeof object[key] === 'object')
+            .forEach(key => object[key] = proxyObject(object[key], parent));
+        return new Proxy(object, socketProxy(parent));
+    }
+
+    function msg (msg) {
+        socket.emit('msg', msg);
+    }
 })();
